6b188563664084196a233cea57e27f4c2ff36b94
shanole <73411129+shanole@users.noreply.github.com>
2021-03-07 13:53:13 -0800

    Passed test2 for Poem object
    Co-authored-by: Jessica R. Williams <jessicarubinwilliams@gmail.com>

--- src/js/poem.js
+++ src/js/poem.js
@@ -1,3 +1,9 @@
-export default function Poem(string) {
-  this.words = string;
+export default class Poem {
+  constructor(string) {
+    this.words = string;
+  }
+
+  findLines() {
+    this.lines = this.words.split( /\n/);
+  }
 }
