b1d5b91967f08b3e97522f8a3c31634288c5d9df
shanole <73411129+shanole@users.noreply.github.com>
2021-03-07 11:53:53 -0800

    Initialize reusable test Poem object
    Co-authored-by: Jessica R. Williams <jessicarubinwilliams@gmail.com>

--- __tests__/poem.test.js
+++ __tests__/poem.test.js
@@ -1,12 +1,19 @@
 import Poem from './../src/js/poem.js'
 
 describe('Poem', () => {
-  test('should correctly create a poem object from a string argument', () => {
-    const poem = new Poem(`I write, erase, rewrite
+  let testPoem;
+
+  beforeEach(() => {
+    testPoem = new Poem(`I write, erase, rewrite
     Erase again, and then
     A poppy blooms.`);
-    expect(poem.words).toEqual(`I write, erase, rewrite
+  });
+
+  test('should correctly create a poem object from a string argument', () => {
+    expect(testPoem.words).toEqual(`I write, erase, rewrite
     Erase again, and then
     A poppy blooms.`)
   });
+
+  test('should separate the string argument into separate lines and put them each into an array')
 });
