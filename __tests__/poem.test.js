0a63a8bbbe894fd4ae9013c6bcaab0c3772f8612
shanole <73411129+shanole@users.noreply.github.com>
2021-03-07 11:58:24 -0800

    Add good fail for test 2 for Poem object
    Co-authored-by: Jessica R. Williams <jessicarubinwilliams@gmail.com>

--- __tests__/poem.test.js
+++ __tests__/poem.test.js
@@ -15,5 +15,7 @@ describe('Poem', () => {
     A poppy blooms.`)
   });
 
-  test('should separate the string argument into separate lines and put them each into an array')
+  test('should separate the string argument into separate lines and put each line into a Poem.lines property that is an array', () => {
+    expect(testPoem.lines).toEqual(["I write, erase, rewrite","Erase again, and then","A poppy blooms."])
+  })
 });
