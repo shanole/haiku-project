a23636ca44ea44097a3b7a1edd753c061c81c39d
shanole <73411129+shanole@users.noreply.github.com>
2021-03-07 10:21:58 -0800

    Initial commit from template

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Document',
+      title: 'Haiku Checker',
       template: './src/index.html',
       inject: 'body'
     })
