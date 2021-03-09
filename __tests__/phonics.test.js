eaea70a99fbb2051ec5fafddc28b54ff243bae6f
shanole <73411129+shanole@users.noreply.github.com>
2021-03-08 20:51:24 -0800

    pass test3 for syllableChecker
    Co-authored-by: Jessica R. Williams <jessicarubinwilliams@gmail.com>

--- __tests__/phonics.test.js
+++ __tests__/phonics.test.js
@@ -3,13 +3,18 @@ import { syllableChecker } from './../src/js/phonics.js'
 describe('syllableChecker', () => {
 
   test('should return the number of vowels', () => {
-    const string = "I write, erase, rewrite";
-    expect(syllableChecker(string)).toEqual(9);
+    const string = "I am a talking dog";
+    expect(syllableChecker(string)).toEqual(6);
   })
 
   test('should not count "y" as a vowel if it occurs at the beginning of a word', () => {
     const string = "Yak bat";
     expect(syllableChecker(string)).toEqual(2);
+  })
+
+  test('should not count the silent "e"s that occur at the end of words after a consonant', () => {
+    const string = "I write, erase, rewrite";
+    expect(syllableChecker(string)).toEqual(6);
   })
 })
 
--- src/js/phonics.js
+++ src/js/phonics.js
@@ -1,18 +1,42 @@
-export function syllableChecker(string) {
+function removePunctuation(string) {
+  const punctuation = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+  const updatedString = string.replaceAll(punctuation, "");
+  return updatedString;
+}
+
+function cleanString(string) {
+  const noPuncString = removePunctuation(string);
+  return noPuncString.toLowerCase();
+}
+
+function isVowel(letter) {
   const vowels = ["a","e","i","o","u","y"]
+  if (vowels.includes(letter)) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+export function syllableChecker(string) {
+  const cleanedString = cleanString(string);
   let vowelCount = 0;
-  const stringLowerCase = string.toLowerCase();
   
   for (let i=0; i<string.length; i++) {
-    if (vowels.includes(stringLowerCase[i])) {
+    if (isVowel(cleanedString[i])) {
       vowelCount ++;
     }
   }
-  const wordsArray = stringLowerCase.split(" ");
+  const wordsArray = cleanedString.split(" ");
   wordsArray.forEach(function(word) {
     if (word[0] === "y") {
+      vowelCount --;
+    } else if (word[word.length-1] === "e" && isVowel(word[word.length-2]) === false) {
       vowelCount --;
     }
   });
   return vowelCount;
 }
+
+
