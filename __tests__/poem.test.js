df340e19ba0a8e65e48de8a8bd9e6b7cf5ee7178
shanole <73411129+shanole@users.noreply.github.com>
2021-03-09 20:36:17 -0800

    Add code to check number of syllables to each line of Poem, not currently passing test
    Co-authored-by: Jessica R. Williams <jessicarubinwilliams@gmail.com>

--- __tests__/poem.test.js
+++ __tests__/poem.test.js
@@ -16,7 +16,7 @@ A poppy blooms.`)
   });
 
   test('should separate the string into separate lines, put each line into a seperate index within an array, and assign the array to a property with the key linesArray', () => {
-    expect(testPoem.linesArray).toEqual(["I write, erase, rewrite","Erase again, and then","A poppy blooms."])
+    expect(testPoem.linesArray).toEqual(["I write, erase, rewrite", "Erase again, and then", "A poppy blooms."])
   })
 
   test('creates a property with the key numberOfLines and the value of linesArray.length', () => {
@@ -29,6 +29,14 @@ is
 not
 a
 haiku`);
+    expect(testPoem.isHaiku()).toEqual(true);
+    expect(notHaiku.isHaiku()).toEqual(false);
+  })
+
+  test('should return true for a 3-line Poem that has 5 syllables in its first line, 7 syllables in its second line, and 5 syllables in its third line, and return false otherwise', () => {
+    let notHaiku = new Poem(`This is an example of something
+that is not a haiku
+hello`);
     expect(testPoem.isHaiku()).toEqual(true);
     expect(notHaiku.isHaiku()).toEqual(false);
   })
--- src/js/poem.js
+++ src/js/poem.js
@@ -1,12 +1,14 @@
+import { syllableChecker } from './phonics.js'
+
 export default class Poem {
   constructor(string) {
     this.words = string;
-    this.linesArray = this.words.split( /\n/);
+    this.linesArray = this.words.split(/\n/);
     this.numberOfLines = this.linesArray.length;
   }
 
   isHaiku() {
-    if (this.numberOfLines === 3) {
+    if (this.numberOfLines === 3 && syllableChecker(this.linesArray[0]) === 5 && syllableChecker(this.linesArray[1]) === 7 && syllableChecker(this.linesArray[2]) === 5) {
       return true;
     } else {
       return false;
